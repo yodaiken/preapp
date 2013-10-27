d9870fa6060e51815a968d1f39af8ed1eee69f72
Aaron Yodaiken <aaronryodaiken@gmail.com>
2013-10-27 15:25:01 -0400

    Fix public files

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from BlockingConnectionPool import BlockingConnectionPool
 
 app = Flask(
     __name__,
-    static_folder=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'public'),
+    static_folder=os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public'),
     static_url_path='/public')
 
 db = SQLAlchemy(app)
